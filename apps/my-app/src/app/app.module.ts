0fb906daa06ffc12014dd116b99ab4b7aece67ff
Anna Savostianova <anna.rosewine@gmail.com>
2021-04-21 11:29:41 +0300

    libs created

--- apps/my-app/src/app/app.module.ts
+++ apps/my-app/src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AuthFeatureModule } from '@auth/feature';
+import { AuthUiModule } from '@auth/ui';
+import { SharedUiModule } from '@shared/ui';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AuthFeatureModule,
+    AuthUiModule,
+    SharedUiModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  projects: ['<rootDir>/apps/my-app', '<rootDir>/apps/api'],
+  projects: [
+    '<rootDir>/apps/my-app',
+    '<rootDir>/apps/api',
+    '<rootDir>/libs/auth/feature',
+    '<rootDir>/libs/auth/ui',
+    '<rootDir>/libs/shared/ui',
+  ],
 };
